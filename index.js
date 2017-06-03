cddd365d8ee3fc12ccf5c118ee6ddf10f14f5876
Ira Nefedova <iraonegina@yandex.ru>
2017-06-03 22:07:54 +0300

    problems 1-5

---
+++ index.js
@@ -0,0 +1,91 @@
+let value = 0;
+let negatives = new Array();
+
+class Calculator {
+  constructor () {
+
+  }
+
+  set(newValue) {
+    value = newValue;
+  }
+
+  setNegative(value) {
+    negatives.push(value);
+  }
+
+  clearNegative() {
+    negatives = [];
+  }
+
+  getNegative() {
+    return negatives;
+  }
+
+  get() {
+    return value;
+  }
+
+  add (numbers) {
+    this.clearNegative();
+    let separator = this.getSeparator(numbers);
+    numbers = this.validation(numbers);
+    const nums = numbers
+      .split(separator);
+
+    for (let i = 0; i < nums.length; i++) {
+      if (this.isNegative(nums[i])) {
+        this.setNegative(nums[i]);
+      }
+    }
+
+    if (this.getNegative().length != 0) {
+       throw new Error('Отрицательные числа недопустимы. ' + this.getNegative().join(' '));
+    }
+
+    let sum = nums
+      .map(this.toNumber)
+      .reduce(this.sum, 0);
+
+    this.set(sum);
+    return this.get();
+
+  }
+
+  isNegative(value) {
+    if (value.indexOf('-') != -1) {
+      return true;
+    }
+    return false;
+  }
+
+  validation(input) {
+    const regExp = /\/\/<.*>\n(.*)/;
+    if (regExp.test(input)) {
+      return input.replace(regExp, '$1');
+    }
+    return input || '0';
+  }
+
+  getSeparator (input) {
+    const regExp = /\/\/<(.*)>\n.*/;
+    if (regExp.test(input)) {
+      return input.replace(regExp, '$1');
+    }
+    return /[\n,]/;
+  }
+
+  toNumber(string) {
+    const number = parseInt(string);
+    if (isNaN(number)) {
+       throw new Error('bad separator');
+    }
+    return number;
+  }
+
+  sum(x, y) {
+    return x + y;
+  }
+}
+
+module.exports = Calculator;
---
+++ test/test.js
@@ -0,0 +1,40 @@
+const expect = require('chai').expect;
+const Calculator = require('../index');
+
+describe('Calculator', () => {
+  var calc;
+
+  before(() => {
+    calc = new Calculator();
+  });
+
+  it('пустой аргумент - ожидаем 0', () => {
+    expect(calc.add()).to.equal(0);
+  });
+  it('1 - ожидаем 1', () => {
+    expect(calc.add('1')).to.equal(1);
+  });
+  it('1,2 - ожидаем 3', () => {
+    expect(calc.add('1,2')).to.equal(3);
+  });
+  it('1,2,3 - ожидаем 6', () => {
+    expect(calc.add('1, 2, 3')).to.equal(6);
+  });
+  it('1\\n2,3 - ожидаем 6', () => {
+    expect(calc.add('1\n2,3')).to.equal(6);
+  });
+  it('1,\\n - ожидаем ошибку', () => {
+    expect(function () {calc.add('1,\n')}).to.throw('bad separator');
+  });
+  it('//<;>\\n1;2 - ожидаем 3', () => {
+    expect(calc.add('//<;>\n1;2')).to.equal(3);
+  });
+  it('//<;>\\n-1;2 - ожидаем ошибку "Отрицательные числа недопустимы. -1"', () => {
+    expect(function () {calc.add('//<;>\n-1;2')}).to.throw('Отрицательные числа недопустимы. -1');
+  });
+  it('//<;>\\n-1;2;-3 - ожидаем ошибку "Отрицательные числа недопустимы. -1 -3"', () => {
+    expect(function () {calc.add('//<;>\n-1;2;-3')}).to.throw('Отрицательные числа недопустимы. -1 -3');
+  });
+
+
+})
